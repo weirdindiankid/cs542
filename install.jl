d174f658ad60fc4c48b9ae97b2e4eb5f8f583a4f
Dharmesh Tarapore <dharmesh@bu.edu>
2017-12-06 07:39:04 -0500

    Series of testing and logging begins here

---
+++ install.jl
@@ -0,0 +1,15 @@
+# Filename: install.jl
+# Author: Dharmesh Tarapore <dharmesh@bu.edu>
+
+Pkg.add("Compat")
+Pkg.add("Distributions")
+Pkg.add("JLD")
+Pkg.add("HDF5")
+Pkg.add("PyPlot")
+Pkg.add("ArgParse")
+
+Pkg.clone("https://github.com/weirdindiankid/CASInterface.jl", "CASInterface")
+Pkg.clone("https://github.com/weirdindiankid/RLESUtils.jl", "RLESUtils")
+
+Pkg.init(); run(`ln -s $(pwd()) $(Pkg.dir("SISLES"))`); Pkg.pin("SISLES"); Pkg.resolve()
+using SISLES; @assert isdefined(:SISLES); @assert typeof(SISLES) === Module
