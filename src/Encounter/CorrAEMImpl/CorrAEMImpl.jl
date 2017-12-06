fb412a0defbb650f9cecaf8945054604be590245
Dharmesh Tarapore <dharmesh@bu.edu>
2017-12-06 04:46:31 -0500

    Series of testing and logging begins here

--- src/Encounter/CorrAEMImpl/CorrAEMImpl.jl
+++ src/Encounter/CorrAEMImpl/CorrAEMImpl.jl
@@ -665,6 +665,8 @@ function update_psi(state1, state2)
         psi = atand(y / x) + 180
     elseif x >= 0 && y <= 0
         psi = atand(y / x) + 360
+    else
+        error("None worked x is ", x, " and y is ", y)
     end
 
     return psi
