05685385d5d618e7e3d165f79cfa8587374d16a9
Dharmesh Tarapore <dharmesh@bu.edu>
2017-12-06 20:45:10 -0500

    Series of testing and logging begins here

--- src/examples/barebones.jl
+++ src/examples/barebones.jl
@@ -61,8 +61,8 @@ AC2_trajectory_ = Vector{Float64}[]
 
 adm_1, adm_2 = sim.parameters.dm
 
-addObserver(adm_1, "adm_init", x -> push!(AC1_trajectory_, x))
-addObserver(adm_2, "adm_init", x -> push!(AC2_trajectory_, x))
+addObserver(adm_1, "adm_simulate", x -> push!(AC1_trajectory_, x))
+addObserver(adm_2, "adm_simulate", x -> push!(AC2_trajectory_, x))
 
 
 simulate(sim, bTCAS = false, sample_number = sample_number)
